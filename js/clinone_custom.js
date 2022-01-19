2549cd7e05066f242d265d55e3948e724164f7d4
Balabek <vonbabayan@gmail.com>
2022-01-19 23:30:45 +0300

    Homepage (fully responsive)

--- js/clinone_custom.js
+++ js/clinone_custom.js
@@ -1,1 +1,98 @@
 /* 𝙲𝚞𝚜𝚝𝚘𝚖 𝙹𝚂 */
+
+$(document).ready(function(){
+
+    // Fancy animation for hamburger menu
+    $('.clin_nav_toggler').on('click', function(){
+        $('.animated-icon1').toggleClass('open');
+    });
+
+
+    // Resource slider
+    $('.resource_slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplaySpeed: 5000,
+        speed: 400,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 700,
+                settings: {
+                    arrows: false,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+
+
+    // Partners Logo slider
+    $('.partner_slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 574,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
